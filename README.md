40b24da0bf53e1d160fed49eec2c72355c3f0270
Christian <christian@anysrc.net>
2016-10-24 22:42:19 +0200

    rewrite

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # zfs-autosnapshot-rotation
-create and rotate zfs snapshots automatically
+
+Create and rotate zfs snapshots automatically.
+
+```
+root@eisenbart# ./zfsautosnap.sh myraidz/files autosnap 3
+[1/6] Delete myraidz/files@autosnap-20161024-164226
+[2/6] Delete myraidz/files@autosnap-20161024-172345
+[3/6] Delete myraidz/files@autosnap-20161024-191540
+[4/6] Delete myraidz/files@autosnap-20161024-192536
+[5/6] Delete myraidz/files@autosnap-20161024-192538
+[6/6] Delete myraidz/files@autosnap-20161024-192637
+Create new snapshot myraidz/files@autosnap-20161024-222403
+Done
+```
+
+- Create snapshots via cron job
+- Delete automatically old snapshots
+
+## Parameters
+
+- ZFS volume name
+- Snapshot prefix
+- Number of concurrent snapshots
+
--- zfsautosnap.sh
+++ zfsautosnap.sh
@@ -1,58 +1,117 @@
-#!/usr/local/bin/bash
+#!/bin/bash
 
-# Path to ZFS executable:
-ZFS=/sbin/zfs
+##
+# original code by aleonard:
+# http://andyleonard.com/2010/04/07/automatic-zfs-snapshot-rotation-on-freebsd/
+#
+# 07/17/2011 - ertug: made it compatible with zfs-fuse which doesn't have .zfs directories
+# 10/24/2016 - cblechert: more checks, timestamps in snapshotname, many small changes
+##
 
-# Parse arguments:
+
+# Parse arguments
+DIR=$(dirname "$0")
+SCRIPT=$(basename "$0")
 TARGET=$1
 SNAP=$2
 COUNT=$3
 
-# Function to display usage:
+# Calculated stuff
+TSTAMP=$(date "+%Y%m%d-%H%M%S")
+
+# Function to display usage
 usage() {
-scriptname=`/usr/bin/basename $0`
-echo "$scriptname: Take and rotate snapshots on a ZFS file system"
-echo
-echo " Usage:"
-echo " $scriptname target snap_name count"
-echo
-echo " target: ZFS file system to act on"
-echo " snap_name: Base name for snapshots, to be followed by a '.' and"
-echo " an integer indicating relative age of the snapshot"
-echo " count: Number of snapshots in the snap_name.number format to"
-echo " keep at one time. Newest snapshot ends in '.0'."
-echo
-exit
+    echo
+    echo "$SCRIPT: Take and rotate snapshots on a ZFS file system"
+    echo
+    echo "  Usage:"
+    echo "  $SCRIPT target snap_name count"
+    echo
+    echo "  target:    name of ZFS file system to act on"
+    echo "  snap_name: Base name for snapshots, to be followed by a '.' and"
+    echo "             an integer indicating relative age of the snapshot"
+    echo "  count:     Number of snapshots in the snap_name.number format to"
+    echo "             keep at one time.  Newest snapshot ends in '.0'."
+    echo
+    exit 1
 }
 
+# Must run as root
+if [ ! "$(id -u)" == "0" ]; then
+    echo "Must run as root!"
+    exit 1
+fi
+
+# Path to ZFS executable:
+ZFS=$(which zfs)
+
+if [ ! -x "$ZFS" ]; then
+    echo "zfs exexutable not found!";
+    exit 1
+fi
+
+# Tank exists
+if [ ! "$(zfs list -H -t filesystem,volume | grep -P "^$TARGET\t" | wc -l)" -eq 1 ]; then
+    echo "Target tank does not exist!"
+    usage
+fi
+
 # Basic argument checks:
-if [ -z $COUNT ] ; then
-usage
+if ! [[ $COUNT =~ ^[0-9]+$ ]]; then
+    echo "count must be a integer!"
+    usage
 fi
 
+# Snapshotname
+if ! [[ $SNAP =~ ^[a-z0-9\-]+$ ]]; then
+    echo "Allowed characters for snapshot name: a-z, 0-9, -"
+    usage
+fi
+
+# No more arguments
 if [ ! -z $4 ] ; then
-usage
+    echo "This script requires exactly 3 arguments!"
+    usage
 fi
 
-# Snapshots are number starting at 0; $max_snap is the highest numbered
-# snapshot that will be kept.
-max_snap=$(($COUNT -1))
+# Securiy check for snap counter
+if [ ! "$COUNT" -ge 0 ]; then
+    echo "count must be greater than or equal to 0"
+    exit 1
+fi
+
+# List all snapshots
+ALLSNAPS=$($ZFS list -H -t snapshot -d 1 "$TARGET" | awk '{print $1}'| grep -P "@$SNAP-[0-9]{8}-[0-9]{6}$" | sort)
+
+# Calculate number of "old" snapshots
+if [ $COUNT -gt 0 ]; then
+    COUNT=$(($COUNT-1))
+fi
+
+OLDSNAPS=$(echo -e "$ALLSNAPS" | head -n -$COUNT)
+NUMOLD=$(echo -e "$OLDSNAPS" | wc -l)
 
 # Clean up oldest snapshot:
-if [ -d /${TARGET}/.zfs/snapshot/${SNAP}.${max_snap} ] ; then
-$ZFS destroy -r ${TARGET}@${SNAP}.${max_snap}
+if [ $NUMOLD -gt 0 ]; then
+    echo "Delete old snapshots"
+    for (( I=1; I<=$NUMOLD; I++ )); do
+        OLDSNAP=$(echo -e "$OLDSNAPS" | tail -n +$I | head -n 1)
+        echo "[$I/$NUMOLD] Delete $OLDSNAP"
+        $ZFS destroy -r "$OLDSNAP"
+    done
+    echo
 fi
-
-# Rename existing snapshots:
-dest=$max_snap
-while [ $dest -gt 0 ] ; do
-src=$(($dest - 1))
-if [ -d /${TARGET}/.zfs/snapshot/${SNAP}.${src} ] ; then
-$ZFS rename -r ${TARGET}@${SNAP}.${src} ${TARGET}@${SNAP}.${dest}
-fi
-dest=$(($dest - 1))
-done
 
 # Create new snapshot:
-$ZFS snapshot -r ${TARGET}@${SNAP}.0
+NEWSNAP="${TARGET}@${SNAP}-${TSTAMP}"
+echo "Create new snapshot $NEWSNAP"
+$ZFS snapshot -r "$NEWSNAP"
+
+echo "Done"
+
+echo
+zfs list -t all -r -o name,creation,used,avail,usedsnap "$TARGET"
+echo
+
+exit 0
 
